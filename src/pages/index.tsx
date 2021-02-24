4efecd0bcac95303c806d4cb4ed3f6f478167e3c
Guilherme Matos <marques.guilherme0067@gmail.com>
2021-02-24 09:30:35 -0300

    NLW#4 Aula 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,30 @@
-import Head from 'next/head';
 import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+import { CompletedChallengs } from "../components/CompletedChallengs";
+import { Countdown } from "../components/Countdown";
+
+import Head from 'next/head';
+
+import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+        <title>Inicio | move.it</title>
       </Head>
-
+      
       <ExperienceBar />
+
+      <section>
+        <div >
+          <Profile />
+          <CompletedChallengs />
+          <Countdown />
+        </div>
+
+        <div></div>
+      </section>
     </div>
   )
 }
